844c62808dba43d87073fd65f19e26d6c0de1832
TheWattsMan <prestonwattsjr@gmail.com>
2020-05-03 13:23:04 -0400

    more changes to test code

--- gr8bond.asm
+++ gr8bond.asm
@@ -1,15 +1,16 @@
 .text
 ci $r0, 0x0001
-ci $r1, 0x0001
-ci $r2, 0x0000
-ci $r3, 0x1010
+ci $r1, 0x0101
+ci $r2, 0x0001
+ci $r3, 0x0001
 ci $r4, 0x1100
 ci $r5, 0x0011
 ii2pp $r0
-dup $r1, $r0
+ii2pp $r2
 pp2ii $r0
-pp2f $r1
-f2i $r1
-f2pp $r1
-pp2ii $r1
+pp2f $r2
+i2f $r0
+f2i $r2
+f2pp $r2
+pp2ii $r2
 trap
